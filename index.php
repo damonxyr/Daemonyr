1799cb701c5f5de64c025843e47ccaa16fd266f5
damonxyr <damonxyr@163.com>
2015-09-29 23:21:30 +0800

    update index.ohp add a poem

---
+++ index.php
@@ -0,0 +1,60 @@
+<html>
+<header>
+	<style scoped="">
+		p {
+			text-align: center;
+		}
+
+		p.title {
+			font-weight: bold;
+			font-size: 22px;
+		}
+
+		div.content {
+			margin: 0 auto;
+			text-align: center;
+		}
+		div.content p{
+			text-align: center;
+			font-size: 16px;
+			font-weight: bold;
+			margin: 5px auto;
+		}
+	</style>
+</header>
+<body>
+<p class="title">The Road Not Taken
+
+<p>
+
+<p>by Robert Frost</p>
+
+<div class="content">
+
+	<p>Two roads diverged in a yellow wood</p>
+	<p>And sorry I could not travel both</p>
+	<p>And be one The traveler, long I stood</p>
+	<p>And looked down one as far as I could</p>
+	<p>To where it bent in the undergrown</p>
+
+	<p>Then took the other, as just as fair</p>
+	<p>And having perhaps the better claim</p>
+	<p>Because it was grassy and wanted wear</p>
+	<p>Though as for that the passing there</p>
+	<p>Had worn them really about the same</p>
+
+	<p>And both that morning equally lay</p>
+	<p>In leaves no step had trodden black</p>
+	<p>Oh, I kept the first for another day!</p>
+	<p>Yet knowing how way leads on to way</p>
+	<p>I doubted if I should even come back</p>
+
+	<p>I shall be telling this with a sigh</p>
+	<p>Somewhere ages and ages hence</p>
+	<p>Two roads diverged in a wood, and I...</p>
+	<p>I took the one less traveled by</p>
+	<p>And that has made all the difference</p>
+
+</div>
+</body>
+</html>
